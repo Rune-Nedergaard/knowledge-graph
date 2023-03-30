dad5640b4a7caa217797ec999e8641e7ded39b58
Rune-Nedergaard <76943893+Rune-Nedergaard@users.noreply.github.com>
2023-03-30 13:26:59 +0200

    working on fine tuning a bert embedding model

--- src/data/extract_entities.py
+++ src/data/extract_entities.py
@@ -3,6 +3,7 @@ import nltk
 from danlp.models import load_flair_ner_model
 from flair.data import Sentence
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 # Download the NLTK Punkt tokenizer if not already downloaded
 nltk.download('punkt')
@@ -14,11 +15,18 @@ output_dir = 'data/entities/'
 # Load the Flair NER model
 flair_model = load_flair_ner_model()
 
-# Process files
-for filename in tqdm(os.listdir(input_dir), desc="Processing files"):
-    if not filename.endswith('.txt'):
-        continue
+# Get list of files to process
+files_to_process = []
+for filename in os.listdir(input_dir):
+    if filename.endswith('.txt'):
+        output_filename = os.path.splitext(filename)[0] + '_entities.txt'
+        if not os.path.exists(os.path.join(output_dir, output_filename)):
+            files_to_process.append(filename)
 
+print(f"Processing {len(files_to_process)} files...")
+
+# Define function to process a single file
+def process_file(filename):
     with open(os.path.join(input_dir, filename), 'r', encoding='utf-8') as f:
         text = f.read()
 
@@ -30,7 +38,6 @@ for filename in tqdm(os.listdir(input_dir), desc="Processing files"):
     for sentence in sentences:
         flair_sentence = Sentence(sentence)
         flair_model.predict(flair_sentence)
-        #print(flair_sentence.to_tagged_string())
 
         for span in flair_sentence.get_spans('ner'):
             if span.tag != 'O':
@@ -41,3 +48,7 @@ for filename in tqdm(os.listdir(input_dir), desc="Processing files"):
     with open(os.path.join(output_dir, output_filename), 'w', encoding='utf-8') as f:
         entity_strings = ['{},{}'.format(ent['text'], ent['type']) for ent in all_entities]
         f.write('\n'.join(entity_strings))
+
+# Process files using a ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=16) as executor:
+    list(tqdm(executor.map(process_file, files_to_process), total=len(files_to_process), desc="Processing files"))
